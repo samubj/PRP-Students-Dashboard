20e4fb63acc1984db48bf2acf27be13b2aaf3a0c
samubj <bjsamukalavathy@gmail.com>
2026-07-21 15:18:52 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import StudentDashboard from "./Components-Dashboard/Student-Dashboard";
+
+function App() {
+  return (
+    <div className="App">
+      <StudentDashboard />
+    </div>
+  );
+}
+
+export default App;
